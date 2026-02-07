cbdaabc72346ad1b36ce345601ecaa9746546e10
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-02-07 23:24:45 +0530

    add js file

---
+++ JS practice/q1.js
@@ -0,0 +1,47 @@
+// 1 to 10 
+//Question: Create two variables (full name + hobby) and print: "My name is ... and ..."
+const fullname = "Tarun Raj Gaur";
+const hobby = "Coding";
+console.log('My name is ${fullname} and ${hobby} is my hobby.');
+
+
+
+//Question: 45 * 2 - 10 ka result print karo.
+console.log(45 * 2 - 10);
+
+
+
+
+//Question: Date object se current year print karo.
+let year = new Date().getFullYear();
+console.log(year);
+
+
+
+//Question: First name aur last name alag store karo, full name print karo
+
+let firstName = "Tarun";
+let lastName = "Gaur";
+console.log(firstName + " " + lastName);
+
+
+//Question: Ek variable banao, print karo, update karo, phir print karo
+
+let city = "Delhi";
+console.log(city);
+city = "Mumbai";
+console.log(city);
+
+//Question: Custom error message console.error() se print karo.
+console.error("This is a custom error message.");
+
+
+//Question: Number store karo aur uska square print karo
+let number = 5;
+let square = number * number;
+console.log(square);
+
+
+//Question: Boolean variable banao aur print karo
+let isStudent = true;
+console.log(isStudent);
